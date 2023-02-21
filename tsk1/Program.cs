5cac8ac4ad156f2f61626b2fa94bc237f7a0ce5b
Максим <zimovets3@yandex.ru>
2023-02-21 12:59:05 +0300

    Homework 6

---
+++ tsk1/Program.cs
@@ -0,0 +1,64 @@
+﻿Console.Write("Введите числа: ");
+int[] numbers = StringToNum(Console.ReadLine());
+PrintNum(numbers);
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+
+void PrintNum(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+
+Console.WriteLine();
+Console.WriteLine($"Количество чисел > 0 = {sum}");
